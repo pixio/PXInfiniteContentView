8e2dc4e103c609a09303a5a5756e206dd095279d
Dave Heyborne <dave.heyborne@gmail.com>
2016-02-18 17:40:47 -0700

    Added Swift example project.
    Signed-off-by: Dave Heyborne <dave.heyborne@gmail.com>

--- Example/PXInfiniteContentView/main.m
+++ Example/PXInfiniteContentView/main.m
@@ -8,10 +8,13 @@
 
 @import UIKit;
 #import "PXAppDelegate.h"
+#import "PXInfiniteContentView_Example-Swift.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXAppDelegate class]));
+        // COMMENT THIS OUT AND USE THE LINE BELOW TO TEST THE SWIFT EXAMPLE
+        //return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
