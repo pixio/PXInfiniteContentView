91a7170f53622cec0a847706ed8dd269b8ab901c
Michael Biggs <mbiggs@pixio.com>
2017-02-08 17:00:35 -0700

    Added nullability specifiers to library code

--- Pod/Classes/PXInfiniteContentInternalView.h
+++ Pod/Classes/PXInfiniteContentInternalView.h
@@ -12,25 +12,25 @@
 @class PXInfiniteContentInternalView;
 
 @protocol PXInfiniteContentInternalViewDelegate <NSObject>
-- (void) internalInfiniteContentView:(PXInfiniteContentInternalView*)infiniteContentView transitionedToIndex:(int)index;
-- (void) internalInfiniteContentView:(PXInfiniteContentInternalView*)infiniteContentView willShowView:(id)view forIndex:(int)index;
+- (void) internalInfiniteContentView:(PXInfiniteContentInternalView* _Nonnull)infiniteContentView transitionedToIndex:(int)index;
+- (void) internalInfiniteContentView:(PXInfiniteContentInternalView* _Nonnull)infiniteContentView willShowView:(UIView * _Nonnull)view forIndex:(int)index;
 @end
 
 @interface PXInfiniteContentInternalView : UIScrollView
 
-- (instancetype) init __attribute__((unavailable("Use one of the other init methods")));
-- (instancetype) initWithFrame:(CGRect)frame __attribute__((unavailable("Use one of the other init methods")));
+- (instancetype _Nonnull) init __attribute__((unavailable("Use one of the other init methods")));
+- (instancetype _Nonnull) initWithFrame:(CGRect)frame __attribute__((unavailable("Use one of the other init methods")));
 
-- (instancetype) initWithLeftView:(id)leftView centerView:(id)centerView rightView:(id)rightView;
+- (instancetype _Nonnull) initWithLeftView:(UIView * _Nullable)leftView centerView:(UIView * _Nullable)centerView rightView:(UIView * _Nullable)rightView;
 
-@property (weak) id<PXInfiniteContentInternalViewDelegate> internalDelegate;
+@property (weak, nullable) id<PXInfiniteContentInternalViewDelegate> internalDelegate;
 
-@property (nonatomic) id leftView;
-@property (nonatomic) id centerView;
-@property (nonatomic) id rightView;
+@property (nonatomic, nullable) UIView* leftView;
+@property (nonatomic, nullable) UIView* centerView;
+@property (nonatomic, nullable) UIView* rightView;
 @property (nonatomic) BOOL shouldBeRequiredToFailByGestureRecognizers;
 @property (nonatomic) int index;
-@property (nonatomic) PXPageIndexBounds* pageIndexBounds;
+@property (nonatomic, nonnull) PXPageIndexBounds* pageIndexBounds;
 @property (nonatomic) CGFloat pagingVelocityThreshold;
 
 @property (nonatomic) BOOL bouncesAtBoundaries;
